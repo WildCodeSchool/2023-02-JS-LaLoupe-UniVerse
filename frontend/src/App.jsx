32040abdb92eff48475bc7f002e32a78084f7147
MarionbDev <marionbaston84@gmail.com>
2023-04-13 17:11:55 +0200

    ajout composant CardArtiste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import SearchBar from "./components/SearchBar";
 import "./App.css";
 import Header from "./components/Header";
+import CardArtiste from "./components/CardArtiste";
 
 function App() {
   return (
     <div>
       <Header />
       <SearchBar />
+      <div className="flex overflow-scroll">
+        <CardArtiste />
+      </div>
     </div>
   );
 }
