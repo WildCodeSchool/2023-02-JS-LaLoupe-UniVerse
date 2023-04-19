09f6cef640dcf7fa9a0d6adf2c26d207b55a564b
MarionbDev <marionbaston84@gmail.com>
2023-04-19 15:26:34 +0200

    fix LF

--- frontend/src/components/CardListArtiste.jsx
+++ frontend/src/components/CardListArtiste.jsx
@@ -1,17 +1,15 @@
 import artisteApi from "../data/artisteApi";
 import CardArtiste from "./CardArtiste";
 
-const artisteData = artisteApi[0];
-
 export default function CardListArtiste() {
   return (
     <div className="flex-col md:ml-[236px] my-10">
       <h1>Artistes</h1>
       <div className="flex gap-3 overflow-x-auto">
-        {artisteData.map((artiste) => (
+        {artisteApi.map((artiste) => (
           <CardArtiste
             key={artiste.id}
-            imgSrc={artiste.images[1].url}
+            imgSrc={artiste.images[0].url}
             artisteName={artiste.name}
           />
         ))}
