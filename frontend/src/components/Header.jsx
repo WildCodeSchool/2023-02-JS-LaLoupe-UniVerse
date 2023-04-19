80b97e783d201a6a4887aa83166fba458f41ef4a
MarionbDev <marionbaston84@gmail.com>
2023-04-19 22:30:13 +0200

    mise a jour header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,12 @@
 export default function Header() {
   return (
-    <header className="  bg-black h-10 sm:h-24 md:h-36  ">
+    <header className=" bg-black h-14 sm:h-24 md:h-40  ">
       <img
-        className=" right-0 top-0.5 opacity-40 h-full  "
+        className="right-0 top-0.5 opacity-40 h-full  "
         src="../src/assets/image.png"
         alt="wave"
       />
+      <div className="degrade ">0</div>
       <h1 className="universe">
         uni<span>V</span>erse
       </h1>
